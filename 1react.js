cc6628a2f5e5002a35a5225fd6b88f6d97660137
Aditya Dey <deyaditya2004@gmail.com>
2024-09-28 00:43:29 +0530

    Added What is ReactJS

--- 1react.js
+++
@@ -1,1 +0,0 @@
-// 1st file of this directory
---
+++ whatisReactJS.js
@@ -0,0 +1,7 @@
+/*
+What is React JS?
+
+A JavaScript Library for building user interfaces.
+
+React JS is a declarative, efficient and flexible JavaScript Library for building reusable UI Components.
+*/
