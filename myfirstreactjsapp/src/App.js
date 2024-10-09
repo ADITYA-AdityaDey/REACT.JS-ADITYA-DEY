81cca7d4c718ab49804a55918048c640a7a772c1
Aditya Dey <deyaditya2004@gmail.com>
2024-10-09 13:13:56 +0530

    Added Creating React Form and Conditionals Rendering

--- myfirstreactjsapp/src/App.js
+++ myfirstreactjsapp/src/App.js
@@ -1,13 +1,15 @@
 import About from "./screens/About";
 import './App.css';
 import Contact from "./screens/Contact";
+import Register from "./screens/Register";
 
 
 
 function App() {
   return <div>
-    <About />
-    <Contact />
+    {/* <About />
+    <Contact /> */}
+    <Register />
     </div>;
 }
 
